d52128fea3be7ab81e6a61e691cd3175546a1a4f
Artur Nomad <artur@sury.pl>
2019-05-30 13:20:18 +0200

    Added other type of constructor.

---
+++ Constructors/Person.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructors
+{
+    class Person
+    {
+
+        int id;
+        string firstname;
+        string lastname;
+        string address;
+        int age;
+        string gender;
+
+        public Person()
+        {
+            id = 1;
+            firstname = "Marcin";
+            lastname = "Kowalski";
+            age = 14;
+
+        }
+
+        public Person(int id, string firstname, string lastname, string address, int age, string gender)
+        {
+            this.id = id;
+            this.firstname = firstname;
+            this.lastname = lastname;
+            this.address = address;
+            this.age = age;
+            this.gender = gender;
+
+        }
+
+        public Person(int id, string firstname, string lastname)
+        {
+            this.id = id;
+            this.firstname = firstname;
+            this.lastname = lastname;
+            address = "Katowice";
+            age = 17;
+
+        }
+
+        public Person(int id, string firstname)
+        {
+            this.id = id;
+            this.firstname = firstname;
+        }
+
+
+
+
+    }
+
+   
+}
