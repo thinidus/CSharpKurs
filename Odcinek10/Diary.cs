d519d90983d9e59516b759360f0f2acc5bff74c2
Artur Nomad <artur@sury.pl>
2019-05-31 10:29:11 +0200

    Test different kind of variables.

---
+++ Odcinek10/Diary.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace Odcinek10
+
+{
+    class Diary
+    {
+        public Diary()
+        {
+            ratings = new List<float>();
+        }
+
+        //Stan - zmienne - pola
+        List<float> ratings;
+
+
+        //Zachowania
+        public void AddRating(float rating)
+        {
+            ratings.Add(rating);
+        }
+
+        public float CaculateAverage()
+        {
+            float sum = 0;
+            float average = 0;
+
+            foreach (var rating in ratings)
+            {
+                sum += rating;
+            }
+
+            average = sum / ratings.Count();
+            return average;
+        }
+
+        public float GiveMaxRating()
+        {
+            return ratings.Max();
+        }
+
+        public float GiveMinRating()
+        {
+            return ratings.Min();
+        }
+
+
+    }
+}
