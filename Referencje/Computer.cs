2588ab2c249b75d0325eaa6d4639ed63c6e111b8
Artur Nomad <artur@sury.pl>
2019-08-23 09:33:24 +0200

    Ref i out

---
+++ Referencje/Computer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Referencje
+{
+    class Computer
+    {
+        public static int wystapienia;
+
+        public Computer()
+        {
+            wystapienia++;
+        }
+
+        public string name = "Stara nazwa";
+
+        public void Showname(string name)
+        {
+            Console.WriteLine(name);
+        }
+
+
+    }
+}
---
+++ Referencje/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Referencje
+{
+    class Program
+    { 
+    public static void ShowName(ref Computer personalComputer)
+    {
+            personalComputer = new Computer();
+            personalComputer.name = "Moja nazwa";
+
+        }
+
+    
+        static void Main(string[] args)
+        {
+
+
+            Computer myComputer = new Computer();
+
+            Computer herComputer = myComputer;
+
+            
+
+            ShowName(ref myComputer);
+
+            Console.WriteLine(myComputer.name + " wystąpienia " + Computer.wystapienia);
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
