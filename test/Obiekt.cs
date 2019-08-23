6c561424d17a42e1eb63234a8caab964c9cbb782
Artur Nomad <artur@sury.pl>
2019-08-23 20:17:17 +0200

    ?

---
+++ test/Obiekt.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    class Obiekt
+    {
+
+    public Obiekt()
+        {
+            wystapienia += 1;
+        }
+
+        public int wystapienia;
+
+        public string nazwa = "Stara nazwa";
+
+        
+
+        public void Zeruj() {
+            nazwa = "";
+        }
+
+
+        
+    }
+}
---
+++ test/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            void ChangeName(ref Obiekt nazwaobiektu)
+            {
+                nazwaobiektu = new Obiekt();
+                nazwaobiektu.nazwa = "nowa nazwa";
+            }
+
+            Obiekt newObiekt = new Obiekt();
+
+            
+
+            Obiekt najnowszy = newObiekt;
+
+            ChangeName(ref najnowszy);
+
+
+
+
+
+            Console.WriteLine(najnowszy.nazwa + " - " + najnowszy.wystapienia + " - " + najnowszy.nazwa.GetType());
+
+
+
+
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
